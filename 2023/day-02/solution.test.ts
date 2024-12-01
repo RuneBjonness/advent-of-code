a2ed7a9d972c1a7d147d5145b77454d6a3fa5d6a
Rune Bjønness <rune.bjonness@gmail.com>
2024-12-01 19:26:26 +0100

    2023 day 2 solution

--- 2023/day-02/solution.test.ts
+++ 2023/day-02/solution.test.ts
@@ -2,14 +2,18 @@ import { expect, test } from "vitest";
 import { silver, gold } from "./solution.ts";
 import { describe } from "node:test";
 
-const testInput = `123`;
+const testInput = `Game 1: 3 blue, 4 red; 1 red, 2 green, 6 blue; 2 green
+Game 2: 1 blue, 2 green; 3 green, 4 blue, 1 red; 1 green, 1 blue
+Game 3: 8 green, 6 blue, 20 red; 5 blue, 4 red, 13 green; 5 green, 1 red
+Game 4: 1 green, 3 red, 6 blue; 3 green, 6 red; 3 green, 15 blue, 14 red
+Game 5: 6 red, 1 blue, 3 green; 2 blue, 1 red, 2 green`;
 
 describe("December 02", () => {
   test("silver", () => {
-    expect(silver(testInput)).toBe(123);
+    expect(silver(testInput)).toBe(8);
   });
 
   test("gold", () => {
-    expect(gold(testInput)).toBe(246);
+    expect(gold(testInput)).toBe(2286);
   });
 });
--- 2023/day-02/solution.ts
+++ 2023/day-02/solution.ts
@@ -2,11 +2,50 @@ import { DayEntry } from "../aoc-day-entry";
 import { input } from "./input";
 
 export const silver = (input: string): number => {
-  return Number(input);
+  return input
+    .split("\n")
+    .map(parseGame)
+    .filter((x) => x.red <= 12 && x.green <= 13 && x.blue <= 14)
+    .reduce((acc, x) => acc + x.id, 0);
 };
 
 export const gold = (input: string): number => {
-  return Number(input) * 2;
+  return input
+    .split("\n")
+    .map(parseGame)
+    .reduce((acc, x) => acc + x.red * x.green * x.blue, 0);
+};
+
+type Game = {
+  id: number;
+  red: number;
+  green: number;
+  blue: number;
+};
+
+const parseGame = (txt: string): Game => {
+  const game: Game = {
+    id: Number(txt.substring(5, txt.indexOf(":"))),
+    red: 0,
+    green: 0,
+    blue: 0,
+  };
+
+  const subsets = txt
+    .substring(txt.indexOf(":") + 1)
+    .split(";")
+    .map((x) => x.trim());
+
+  for (const subset of subsets) {
+    const colors = subset.split(", ");
+
+    for (const color of colors) {
+      const [count, colorName] = color.split(" ");
+      game[colorName] = Math.max(game[colorName], Number(count));
+    }
+  }
+
+  return game;
 };
 
 export const day02: DayEntry = {
