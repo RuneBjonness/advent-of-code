a1eab7098814e146e8aa612322c4c99f08b35013
Rune Bjønness <rune.bjonness@gmail.com>
2024-12-01 11:47:45 +0100

    Day 1 solution

--- 2024/day-01/solution.test.ts
+++ 2024/day-01/solution.test.ts
@@ -2,12 +2,19 @@ import { expect, test } from "vitest";
 import { silver, gold } from "./solution.ts";
 import { describe } from "node:test";
 
+const testInput = `3   4
+4   3
+2   5
+1   3
+3   9
+3   3`;
+
 describe("December 01", () => {
   test("silver", () => {
-    expect(silver("123")).toBe(123);
+    expect(silver(testInput)).toBe(11);
   });
 
   test("gold", () => {
-    expect(gold("123")).toBe(246);
+    expect(gold(testInput)).toBe(31);
   });
 });
