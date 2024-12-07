6ea6f534149bd2aa9682b457f8296a8a8d22aad7
Rune Bjønness <rune.bjonness@gmail.com>
2024-12-07 12:03:31 +0100

    2024 day 7 solution

---
+++ 2024/day-07/solution.ts
@@ -0,0 +1,78 @@
+import { DayEntry } from "../aoc-day-entry";
+import { input } from "./input";
+
+export const silver = (input: string): number => {
+  return totalCalibrationResult(input, [add, multiply]);
+};
+
+export const gold = (input: string): number => {
+  return totalCalibrationResult(input, [add, multiply, concatinate]);
+};
+
+type Operation = (a: number, b: number) => number;
+const add: Operation = (a, b) => a + b;
+const multiply: Operation = (a, b) => a * b;
+const concatinate: Operation = (a, b) => Number(`${a}${b}`);
+
+const totalCalibrationResult = (
+  input: string,
+  operations: Operation[]
+): number => {
+  return input
+    .split("\n")
+    .map((line) => {
+      const [target, valueLine] = line.split(": ");
+      const targetValue = Number(target);
+      const values = valueLine.split(" ").map(Number);
+
+      if (values.length < 2) {
+        return 0;
+      }
+
+      return isEquationSolvable(
+        values[0],
+        values[1],
+        operations,
+        values.slice(2),
+        targetValue
+      )
+        ? targetValue
+        : 0;
+    })
+    .reduce((acc, value) => acc + value, 0);
+};
+
+const isEquationSolvable = (
+  a: number,
+  b: number,
+  operations: Operation[],
+  remainingValues: number[],
+  target: number
+): boolean => {
+  const accumulatedValues = operations.map((operation) => operation(a, b));
+
+  if (remainingValues.length === 0) {
+    return accumulatedValues.includes(target);
+  }
+
+  const validValues = accumulatedValues.filter((value) => value <= target);
+
+  if (validValues.length === 0) {
+    return false;
+  }
+
+  return validValues.some((value) => {
+    return isEquationSolvable(
+      value,
+      remainingValues[0],
+      operations,
+      remainingValues.slice(1),
+      target
+    );
+  });
+};
+
+export const day07: DayEntry = {
+  silver: () => silver(input),
+  gold: () => gold(input),
+};
--- 2024/index.ts
+++ 2024/index.ts
@@ -4,8 +4,9 @@ import { day03 } from "./day-03/solution";
 import { day04 } from "./day-04/solution";
 import { day05 } from "./day-05/solution";
 import { day06 } from "./day-06/solution";
+import { day07 } from "./day-07/solution";
 
-const dayEntries = [day01, day02, day03, day04, day05, day06];
+const dayEntries = [day01, day02, day03, day04, day05, day06, day07];
 
 const parseDay = (day: string): number => {
   const dayNumber = Number(day);
@@ -22,12 +23,25 @@ console.log("Solving for December ", day);
 
 if (partFilter === "silver" || !partFilter) {
   console.log("--- Silver ---");
+  performance.mark("start silver");
   console.log(dayEntries[day - 1].silver());
+  performance.mark("end silver");
   console.log("\n");
 }
 
 if (partFilter === "gold" || !partFilter) {
   console.log("--- Gold ---");
+  performance.mark("start gold");
   console.log(dayEntries[day - 1].gold());
+  performance.mark("end gold");
   console.log("\n");
 }
+
+performance.measure("silver", "start silver", "end silver");
+performance.measure("gold", "start gold", "end gold");
+
+console.log(
+  performance
+    .getEntriesByType("measure")
+    .map((m) => `${m.name}: ${m.duration.toFixed(1)} ms`)
+);
