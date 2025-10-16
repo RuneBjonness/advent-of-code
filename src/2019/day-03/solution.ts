6569641e6f166675e11fb4474f5a02a02989399b
Rune Bjønness <rune.bjonness@gmail.com>
2025-10-16 21:46:06 +0200

    2019 day 3: improve performance

--- src/2019/day-03/solution.ts
+++ src/2019/day-03/solution.ts
@@ -9,14 +9,18 @@ export const silver = (input: string): number => {
   const path1 = calculateWirePath(wire1Instructions);
   const path2 = calculateWirePath(wire2Instructions);
 
-  for (const point of path1) {
-    if (path2.includes(point)) {
-      const [x, y] = point.split(",").map(Number);
-      const manhattanDist = Math.abs(x) + Math.abs(y);
-      closestIntersectionDist = Math.min(
-        closestIntersectionDist,
-        manhattanDist
-      );
+  for (const s1 of path1) {
+    for (const s2 of path2) {
+      const intersection = findIntersection(s1, s2) ?? findIntersection(s2, s1);
+
+      if (intersection) {
+        const manhattanDist =
+          Math.abs(intersection.x) + Math.abs(intersection.y);
+        closestIntersectionDist = Math.min(
+          closestIntersectionDist,
+          manhattanDist
+        );
+      }
     }
   }
 
@@ -31,13 +35,16 @@ export const gold = (input: string): number => {
   const path1 = calculateWirePath(wire1Instructions);
   const path2 = calculateWirePath(wire2Instructions);
 
-  for (const point of path1) {
-    if (path2.includes(point)) {
-      const wireLength = path1.indexOf(point) + path2.indexOf(point) + 2;
-      closestIntersectionWireLength = Math.min(
-        closestIntersectionWireLength,
-        wireLength
-      );
+  for (const s1 of path1) {
+    for (const s2 of path2) {
+      const intersection = findIntersection(s1, s2) ?? findIntersection(s2, s1);
+
+      if (intersection) {
+        closestIntersectionWireLength = Math.min(
+          closestIntersectionWireLength,
+          intersection.wireLength
+        );
+      }
     }
   }
 
@@ -46,33 +53,79 @@ export const gold = (input: string): number => {
 
 export const day03 = new AocPuzzle(2019, 3, silver, gold, input);
 
-const calculateWirePath = (wireInstructions: string): string[] => {
-  const wirePath: string[] = [];
-  let currentPosition = { x: 0, y: 0 };
+type Position = { x: number; y: number };
 
-  const instructions = wireInstructions.split(",");
-  for (const instruction of instructions) {
+type PathSection = {
+  from: Position;
+  to: Position;
+  direction: string;
+  accumulatedLength: number;
+};
+
+type Intersection = Position & { wireLength: number };
+
+const calculateWirePath = (wireInstructions: string): PathSection[] => {
+  const wirePath: PathSection[] = [];
+  let currentPosition = { x: 0, y: 0 };
+  let accumulatedLength = 0;
+
+  for (const instruction of wireInstructions.split(",")) {
     const direction = instruction.charAt(0);
     const distance = Number(instruction.slice(1));
+    const newPosition = { ...currentPosition };
 
-    for (let i = 0; i < distance; i++) {
-      switch (direction) {
-        case "R":
-          currentPosition.x += 1;
-          break;
-        case "L":
-          currentPosition.x -= 1;
-          break;
-        case "U":
-          currentPosition.y -= 1;
-          break;
-        case "D":
-          currentPosition.y += 1;
-          break;
-      }
-      wirePath.push(`${currentPosition.x},${currentPosition.y}`);
+    switch (direction) {
+      case "R":
+        newPosition.x += distance;
+        break;
+      case "L":
+        newPosition.x -= distance;
+        break;
+      case "U":
+        newPosition.y -= distance;
+        break;
+      case "D":
+        newPosition.y += distance;
+        break;
     }
+
+    wirePath.push({
+      from: currentPosition,
+      to: newPosition,
+      direction,
+      accumulatedLength,
+    });
+    currentPosition = newPosition;
+    accumulatedLength += distance;
   }
 
   return wirePath;
+};
+
+const findIntersection = (
+  horizontal: PathSection,
+  vertical: PathSection
+): Intersection | null => {
+  if (
+    (horizontal.direction === "R" || horizontal.direction === "L") &&
+    (vertical.direction === "U" || vertical.direction === "D")
+  ) {
+    if (
+      Math.min(horizontal.from.x, horizontal.to.x) < vertical.from.x &&
+      vertical.from.x < Math.max(horizontal.from.x, horizontal.to.x) &&
+      Math.min(vertical.from.y, vertical.to.y) < horizontal.from.y &&
+      horizontal.from.y < Math.max(vertical.from.y, vertical.to.y)
+    ) {
+      return {
+        x: vertical.from.x,
+        y: horizontal.from.y,
+        wireLength:
+          horizontal.accumulatedLength +
+          vertical.accumulatedLength +
+          Math.abs(vertical.from.x - horizontal.from.x) +
+          Math.abs(horizontal.from.y - vertical.from.y),
+      };
+    }
+  }
+  return null;
 };
--- src/2019/index.ts
+++ src/2019/index.ts
@@ -2,8 +2,4 @@ import { day01 } from "./day-01/solution";
 import { day02 } from "./day-02/solution";
 import { day03 } from "./day-03/solution";
 
-export const puzzles2019 = [
-  day01,
-  day02,
-  day03.skip("silver", "32 seconds").skip("gold", "32 seconds"),
-];
+export const puzzles2019 = [day01, day02, day03];
