5609219d8f6a6b76bf48d1cad67a03b11b689445
Rune Bjønness <rune.bjonness@gmail.com>
2025-12-12 23:58:59 +0100

    2025 day 9 part 1

--- src/2025/day-09/solution.test.ts
+++ src/2025/day-09/solution.test.ts
@@ -1,22 +1,29 @@
 import { describe, expect, test } from "bun:test";
 import { day09 } from "./solution";
 
-const testInput = `0`;
+const testInput = `7,1
+11,1
+11,7
+9,7
+9,5
+2,5
+2,3
+7,3`;
 
 describe("December 09", () => {
   test("silver - test input", () => {
-    expect(day09.silver(testInput)).toBe(0);
+    expect(day09.silver(testInput)).toBe(50);
   });
 
   test("silver - actual puzzle input", async () => {
-    expect(day09.silver(await day09.readInput())).toBe(0);
+    expect(day09.silver(await day09.readInput())).toBe(4740155680);
   });
 
-  test("gold - test input", () => {
-    expect(day09.gold(testInput)).toBe(0);
-  });
+  // test("gold - test input", () => {
+  //   expect(day09.gold(testInput)).toBe(24);
+  // });
 
-  test("gold - actual puzzle input", async () => {
-    expect(day09.gold(await day09.readInput())).toBe(0);
-  });
+  // test("gold - actual puzzle input", async () => {
+  //   expect(day09.gold(await day09.readInput())).toBe(0);
+  // });
 });
--- src/2025/day-09/solution.ts
+++ src/2025/day-09/solution.ts
@@ -1,13 +1,27 @@
 import { AocPuzzle } from "@/aoc-puzzle";
+import { vec2, Vec2 } from "@/lib/vec2";
 
 const silver = (input: string): number => {
-  let result = 0;
-  return result;
+  const positions: Vec2[] = input
+    .split("\n")
+    .map((line) => vec2(...line.split(",").map(Number)));
+
+  let maxArea = 0;
+  for (let a = 0; a < positions.length - 1; a++) {
+    for (let b = a + 1; b < positions.length; b++) {
+      const area =
+        (Math.abs(positions[b].x - positions[a].x) + 1) *
+        (Math.abs(positions[b].y - positions[a].y) + 1);
+      if (area > maxArea) {
+        maxArea = area;
+      }
+    }
+  }
+  return maxArea;
 };
 
 const gold = (input: string): number => {
-  let result = 0;
-  return result;
+  return NaN;
 };
 
 export const day09 = new AocPuzzle(2025, 9, silver, gold);
