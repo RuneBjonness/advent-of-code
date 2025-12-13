889983204597e8c2532332ad5770aae7bddcf30b
Rune Bjønness <rune.bjonness@gmail.com>
2025-12-13 18:23:54 +0100

    2025 day 10 part 1

--- src/2025/day-10/solution.test.ts
+++ src/2025/day-10/solution.test.ts
@@ -1,22 +1,24 @@
 import { describe, expect, test } from "bun:test";
 import { day10 } from "./solution";
 
-const testInput = ``;
+const testInput = `[.##.] (3) (1,3) (2) (2,3) (0,2) (0,1) {3,5,4,7}
+[...#.] (0,2,3,4) (2,3) (0,4) (0,1,2) (1,2,3,4) {7,5,12,7,2}
+[.###.#] (0,1,2,3,4) (0,3,4) (0,1,2,4,5) (1,2) {10,11,11,5,10,5}`;
 
 describe("December 10", () => {
   test("silver - test input", () => {
-    expect(day10.silver(testInput)).toBe(0);
+    expect(day10.silver(testInput)).toBe(7);
   });
 
   test("silver - actual puzzle input", async () => {
-    expect(day10.silver(await day10.readInput())).toBe(0);
+    expect(day10.silver(await day10.readInput())).toBe(486);
   });
 
   test("gold - test input", () => {
-    expect(day10.gold(testInput)).toBe(0);
+    expect(day10.gold(testInput)).toBe(33);
   });
 
-  test("gold - actual puzzle input", async () => {
-    expect(day10.gold(await day10.readInput())).toBe(0);
-  });
+  // test("gold - actual puzzle input", async () => {
+  //   expect(day10.gold(await day10.readInput())).toBe(0);
+  // });
 });
--- src/2025/day-10/solution.ts
+++ src/2025/day-10/solution.ts
@@ -1,7 +1,15 @@
 import { AocPuzzle } from "@/aoc-puzzle";
 
 const silver = (input: string): number => {
+  const machines = input.split("\n").map((m) => m.split(" "));
   let result = 0;
+  for (const machine of machines) {
+    const target = getTarget(machine);
+    const buttons = getButtons(machine);
+    const minButtons = minButtonsToTarget(buttons, target);
+    result += minButtons;
+  }
+
   return result;
 };
 
@@ -11,3 +19,56 @@ const gold = (input: string): number => {
 };
 
 export const day10 = new AocPuzzle(2025, 10, silver, gold);
+
+const minButtonsToTarget = (
+  buttons: number[],
+  target: number,
+  currentButtons = 0,
+  currentMin = Infinity
+): number => {
+  if (target === 0) {
+    return currentButtons;
+  }
+
+  currentButtons++;
+
+  if (buttons.length === 0 || currentMin <= currentButtons) {
+    return Infinity;
+  }
+
+  const buttonsToConsider = buttons.filter((b) => (b & target) !== 0);
+  for (const button of buttonsToConsider) {
+    const val = minButtonsToTarget(
+      buttons.toSpliced(buttons.indexOf(button), 1),
+      target ^ button,
+      currentButtons,
+      currentMin
+    );
+    currentMin = Math.min(currentMin, val);
+  }
+
+  return currentMin;
+};
+
+const getTarget = (machine: string[]): number => {
+  return machine[0]
+    .slice(1, -1)
+    .split("")
+    .reverse()
+    .map((c) => (c === "#" ? 1 : 0))
+    .reduce((acc, bit) => (acc << 1) | bit, 0);
+};
+
+const getButtons = (machine: string[]): number[] => {
+  const buttons: number[] = [];
+  for (let i = 1; i < machine.length - 1; i++) {
+    buttons.push(
+      machine[i]
+        .slice(1, -1)
+        .split(",")
+        .map((n) => 2 ** Number(n))
+        .reduce((acc, bit) => acc | bit, 0)
+    );
+  }
+  return buttons;
+};
