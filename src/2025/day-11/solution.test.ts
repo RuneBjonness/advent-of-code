1d42a7a26e06b4753bb241bda82b30ecce37298b
Rune Bjønness <rune.bjonness@gmail.com>
2025-12-20 23:13:31 +0100

    2025 day 11

--- src/2025/day-11/solution.test.ts
+++ src/2025/day-11/solution.test.ts
@@ -1,22 +1,45 @@
 import { describe, expect, test } from "bun:test";
 import { day11 } from "./solution";
 
-const testInput = ``;
+const testInput = `aaa: you hhh
+you: bbb ccc
+bbb: ddd eee
+ccc: ddd eee fff
+ddd: ggg
+eee: out
+fff: out
+ggg: out
+hhh: ccc fff iii
+iii: out`;
+
+const testInputGold = `svr: aaa bbb
+aaa: fft
+fft: ccc
+bbb: tty
+tty: ccc
+ccc: ddd eee
+ddd: hub
+hub: fff
+eee: dac
+dac: fff
+fff: ggg hhh
+ggg: out
+hhh: out`;
 
 describe("December 11", () => {
   test("silver - test input", () => {
-    expect(day11.silver(testInput)).toBe(0);
+    expect(day11.silver(testInput)).toBe(5);
   });
 
   test("silver - actual puzzle input", async () => {
-    expect(day11.silver(await day11.readInput())).toBe(0);
+    expect(day11.silver(await day11.readInput())).toBe(758);
   });
 
   test("gold - test input", () => {
-    expect(day11.gold(testInput)).toBe(0);
+    expect(day11.gold(testInputGold)).toBe(2);
   });
 
   test("gold - actual puzzle input", async () => {
-    expect(day11.gold(await day11.readInput())).toBe(0);
+    expect(day11.gold(await day11.readInput())).toBe(490695961032000);
   });
 });
--- src/2025/day-11/solution.ts
+++ src/2025/day-11/solution.ts
@@ -1,13 +1,100 @@
 import { AocPuzzle } from "@/aoc-puzzle";
 
 const silver = (input: string): number => {
-  let result = 0;
-  return result;
+  return countPaths(parseDevices(input), "you", "out");
 };
 
 const gold = (input: string): number => {
-  let result = 0;
-  return result;
+  return (
+    countPathsWithNodes(parseDevices(input), "svr", "out", ["fft", "dac"]).find(
+      (item) => item.requiredDevices === 2
+    )?.pathCount || 0
+  );
 };
 
-export const day11 = new AocPuzzle(2025, 11, silver, gold);
+const both = (input: string): [number, number] => {
+  const devices = parseDevices(input);
+  return [
+    countPaths(devices, "you", "out"),
+    countPathsWithNodes(devices, "svr", "out", ["fft", "dac"]).find(
+      (item) => item.requiredDevices === 2
+    )?.pathCount || 0,
+  ];
+};
+
+export const day11 = new AocPuzzle(2025, 11, silver, gold, both);
+
+const parseDevices = (input: string): Record<string, string[]> => {
+  const devices: Record<string, string[]> = {};
+  const lines = input.split("\n").map((line) => line.split(": "));
+  for (const [device, connections] of lines) {
+    devices[device] = connections.split(" ");
+  }
+  return devices;
+};
+
+const countPaths = (
+  devices: Record<string, string[]>,
+  current: string,
+  target: string
+): number => {
+  if (current === target) {
+    return 1;
+  }
+  let pathCount = 0;
+  for (const output of devices[current] || []) {
+    pathCount += countPaths(devices, output, target);
+  }
+  return pathCount;
+};
+
+type PathCount = { pathCount: number; requiredDevices: number };
+
+const countPathsWithNodes = (
+  devices: Record<string, string[]>,
+  current: string,
+  target: string,
+  requiredDevices: string[],
+  pathCache: Record<string, PathCount[]> = {}
+): PathCount[] => {
+  if (current === target) {
+    return [{ pathCount: 1, requiredDevices: 0 }];
+  }
+  if (pathCache[current]) {
+    return pathCache[current];
+  }
+
+  let pathCounts: PathCount[] = [];
+  for (const output of devices[current] || []) {
+    pathCounts.push(
+      ...countPathsWithNodes(
+        devices,
+        output,
+        target,
+        requiredDevices,
+        pathCache
+      )
+    );
+  }
+
+  pathCounts = pathCounts.reduce<PathCount[]>((acc, curr) => {
+    const existing = acc.find(
+      (item) => item.requiredDevices === curr.requiredDevices
+    );
+    if (existing) {
+      existing.pathCount += curr.pathCount;
+    } else {
+      acc.push({ ...curr });
+    }
+    return acc;
+  }, []);
+
+  if (requiredDevices.includes(current)) {
+    for (const pathCount of pathCounts) {
+      pathCount.requiredDevices += 1;
+    }
+  }
+
+  pathCache[current] = pathCounts;
+  return pathCounts;
+};
