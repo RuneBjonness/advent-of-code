43505845397b392800208121b3c2a648573e4a4f
Rune Bjønness <rune.bjonness@gmail.com>
2025-12-28 00:33:22 +0100

    2025 day 10 part 2: Working solution

--- src/2025/day-10/solution.ts
+++ src/2025/day-10/solution.ts
@@ -16,49 +16,15 @@ const silver = (input: string): number => {
 const gold = (input: string): number => {
   const machines = input.split("\n").map((m) => m.split(" "));
   let result = 0;
-  let idx = 0;
-  let debug = {
-    solved: 0,
-    unsolved: 0,
-    solvedByMaxPressesStrategy: 0,
-    solveByDecreasingPressesStrategy: 0,
-  };
   for (const machine of machines) {
-    idx++;
-    // console.log(`Processing machine ${idx}/${machines.length}`);
     const target = getJoltageTarget(machine);
-    const buttons = getSortedButtons(
+    const combinations = calculateButtonCombinations(
       machine.slice(1, -1).map(getButton),
-      target
+      target.length
     );
-
-    let minButtons = solveJoltageTargetByMaxPressesStrategy(buttons, [
-      ...target,
-    ]);
-    if (minButtons < Infinity) {
-      // console.log(`  Solved by max presses strategy!`);
-      debug.solvedByMaxPressesStrategy++;
-    } else {
-      minButtons = solveJoltageTargetByDecreasingPressesStrategy(buttons, [
-        ...target,
-      ]);
-      if (minButtons < Infinity) {
-        // console.log(`  Solved by decreasing presses strategy!`);
-        debug.solveByDecreasingPressesStrategy++;
-      }
-    }
-
-    if (minButtons === Infinity) {
-      // console.log(`  No solution found for machine ${idx}.`);
-      debug.unsolved++;
-      continue;
-    }
-
-    debug.solved++;
+    const minButtons = minimumButtonsToJoltageTarget(target, combinations);
     result += minButtons;
-    // console.log(`  Minimum buttons: ${minButtons}, result so far: ${result}`);
   }
-  // console.log(`Debug info: ${JSON.stringify(debug)}`);
   return result;
 };
 
@@ -119,282 +85,78 @@ const getJoltageTarget = (machine: string[]): number[] => {
   return machine.at(-1).slice(1, -1).split(",").map(Number);
 };
 
-const remainingJoltageTarget = (
-  target: number[],
-  button: number[],
-  presses = 1
-): number[] => {
-  for (let i = 0; i < button.length; i++) {
-    target[button[i]] -= presses;
-  }
-  return target;
+type ButtonCombinations = {
+  totalPresses: number;
+  effect: number[];
 };
-
-type ButtonInfo = {
-  maxPresses: number;
-  button: number[];
-};
-
-const calculateMaxPresses = (button: number[], target: number[]): number => {
-  return Math.min(...button.map((i) => target[i]));
-};
-
-const getSortedButtons = (
+const calculateButtonCombinations = (
   buttons: number[][],
-  target: number[]
-): ButtonInfo[] => {
-  return buttons
-    .map((btn) => ({
-      maxPresses: calculateMaxPresses(btn, target),
-      button: btn,
-    }))
-    .filter((b) => b.maxPresses > 0)
-    .sort(
-      (a, b) => b.button.length - a.button.length || b.maxPresses - a.maxPresses
+  targetLength: number,
+  startIndex: number = 0,
+  currentCombination: number[] = [],
+  currentEffect: number[] = Array.from({ length: targetLength }, () => 0),
+  totalPresses: number = 0,
+  combinations: Record<string, ButtonCombinations[]> = {}
+): Record<string, ButtonCombinations[]> => {
+  const key = currentEffect.map((x) => (x % 2 === 0 ? "0" : "1")).join("");
+  if (!combinations[key]) {
+    combinations[key] = [];
+  }
+  combinations[key].push({ totalPresses, effect: [...currentEffect] });
+
+  for (let i = startIndex; i < buttons.length; i++) {
+    const button = buttons[i];
+    const newEffect = [...currentEffect];
+    for (const index of button) {
+      newEffect[index] = (newEffect[index] ?? 0) + 1;
+    }
+    calculateButtonCombinations(
+      buttons,
+      targetLength,
+      i + 1,
+      [...currentCombination, i],
+      newEffect,
+      totalPresses + 1,
+      combinations
     );
+  }
+  return combinations;
 };
 
-const pressAllButtonsWhereTargetIsEqualToTotalEffect = (
-  btns: ButtonInfo[],
-  target: number[],
-  currentButtons = 0
-): {
-  buttons: ButtonInfo[];
-  target: number[];
-  currentButtons: number;
-  solved: boolean;
-  unsolvable: boolean;
-} => {
-  const result = {
-    buttons: btns
-      .map((b) => ({
-        maxPresses: calculateMaxPresses(b.button, target),
-        button: [...b.button],
-      }))
-      .filter((b) => b.maxPresses > 0),
-    target: [...target],
-    currentButtons,
-    solved: false,
-    unsolvable: false,
-  };
-
-  for (let i = 0; i < result.target.length; i++) {
-    if (result.target[i] === 0) {
-      continue;
-    }
-    const totalEffect = result.buttons
-      .map((b) =>
-        result.target.map((_, i) => (b.button.includes(i) ? b.maxPresses : 0))
-      )
-      .reduce((acc, be) => acc + be[i], 0);
-    if (totalEffect < result.target[i]) {
-      result.unsolvable = true;
-      return result;
-    }
-    if (totalEffect === result.target[i]) {
-      for (const button of result.buttons.filter((b) => b.button.includes(i))) {
-        result.target = remainingJoltageTarget(
-          [...result.target],
-          button.button,
-          button.maxPresses
-        );
-        result.currentButtons += button.maxPresses;
-        button.maxPresses = 0;
-      }
-      if (result.target.some((t) => t < 0)) {
-        result.unsolvable = true;
-        return result;
-      }
-    }
-  }
-
-  if (result.target.every((t) => t === 0)) {
-    result.currentButtons = currentButtons;
-    result.solved = true;
-  }
-  return result;
+const getTargetParityKey = (target: number[]): string => {
+  return target.map((x) => (x % 2 === 0 ? "0" : "1")).join("");
 };
 
-const solveJoltageTargetByMaxPressesStrategy = (
-  btns: ButtonInfo[],
+const minimumButtonsToJoltageTarget = (
   target: number[],
-  currentButtons = 0,
-  currentMin = Infinity,
-  theoreticalMin = Math.max(...target)
-): number => {
-  if (target.every((t) => t === 0)) {
-    return currentButtons;
-  }
-  if (target.some((t) => t < 0)) {
-    return Infinity;
-  }
-  if (currentMin <= currentButtons + 1) {
-    return Infinity;
-  }
-  const result = pressAllButtonsWhereTargetIsEqualToTotalEffect(
-    btns,
-    target,
-    currentButtons
-  );
-  if (result.solved) {
-    return result.currentButtons;
-  } else if (result.unsolvable) {
-    return Infinity;
-  }
-  const buttons = result.buttons;
-  target = result.target;
-  currentButtons = result.currentButtons;
-
-  for (const button of buttons) {
-    const newTarget = remainingJoltageTarget(
-      [...target],
-      button.button,
-      button.maxPresses
-    );
-    const val = solveJoltageTargetByMaxPressesStrategy(
-      [...buttons],
-      [...newTarget],
-      currentButtons + button.maxPresses,
-      currentMin,
-      theoreticalMin
-    );
-    currentMin = Math.min(currentMin, val);
-    if (currentMin === theoreticalMin) {
-      break;
-    }
-  }
-  return currentMin;
-};
-
-const solveJoltageTargetByDecreasingPressesStrategy = (
-  buttons: ButtonInfo[],
-  target: number[],
-  currentButtons = 0,
-  currentMin = Infinity,
-  theoreticalMin = Math.max(...target),
-  firstCall = true,
+  combinations: Record<string, ButtonCombinations[]>,
   targetCache = new Map<string, number>()
 ): number => {
-  const targetKey = target.join(",");
-  if (targetCache.has(targetKey)) {
-    return targetCache.get(targetKey)! + currentButtons;
+  const targetCacheKey = target.join(",");
+  if (targetCache.has(targetCacheKey)) {
+    return targetCache.get(targetCacheKey)!;
   }
-
   if (target.every((t) => t === 0)) {
-    return currentButtons;
+    return 0;
   }
   if (target.some((t) => t < 0)) {
     return Infinity;
   }
-  if (currentMin <= currentButtons + 1) {
+
+  const targetKey = getTargetParityKey(target);
+  if (!combinations[targetKey]) {
     return Infinity;
   }
 
-  if (!firstCall) {
-    const result = pressAllButtonsWhereTargetIsEqualToTotalEffect(
-      buttons,
-      target,
-      currentButtons
+  let currentMin = Infinity;
+  for (const combo of combinations[targetKey]) {
+    const res = minimumButtonsToJoltageTarget(
+      target.map((t, i) => (t - combo.effect[i]) / 2),
+      combinations,
+      targetCache
     );
-    if (result.solved) {
-      return result.currentButtons;
-    } else if (result.unsolvable) {
-      return Infinity;
-    }
-    buttons = result.buttons;
-    target = result.target;
-    currentButtons = result.currentButtons;
-
-    for (const button of buttons) {
-      const newTarget = remainingJoltageTarget(
-        [...target],
-        button.button,
-        button.maxPresses
-      );
-      const val = solveJoltageTargetByDecreasingPressesStrategy(
-        [...buttons],
-        [...newTarget],
-        currentButtons + button.maxPresses,
-        currentMin,
-        theoreticalMin,
-        false,
-        targetCache
-      );
-      targetCache.set(
-        newTarget.join(","),
-        val - currentButtons - button.maxPresses
-      );
-      currentMin = Math.min(currentMin, val);
-      if (currentMin === theoreticalMin) {
-        break;
-      }
-    }
+    currentMin = Math.min(currentMin, res * 2 + combo.totalPresses);
   }
-
-  if (currentMin === Infinity && buttons.length > 1 && firstCall) {
-    for (const button of buttons) {
-      const buttonsToTry = buttons.toSpliced(buttons.indexOf(button), 1);
-      let pressesToTry = button.maxPresses - 1;
-      const newTarget = remainingJoltageTarget(
-        [...target],
-        button.button,
-        pressesToTry
-      );
-      while (pressesToTry >= 1) {
-        const val = solveJoltageTargetByDecreasingPressesStrategy(
-          buttonsToTry,
-          [...newTarget],
-          currentButtons + pressesToTry,
-          currentMin,
-          theoreticalMin,
-          false,
-          targetCache
-        );
-        targetCache.set(
-          newTarget.join(","),
-          val - currentButtons - pressesToTry
-        );
-        currentMin = Math.min(currentMin, val);
-        if (currentMin < Infinity) {
-          return currentMin;
-        }
-        pressesToTry--;
-      }
-    }
-
-    let pressesToSubtractFromMax = 2;
-    while (pressesToSubtractFromMax < 500) {
-      for (const button of buttons) {
-        const buttonsToTry = buttons.toSpliced(buttons.indexOf(button), 1);
-        let pressesToTry = button.maxPresses - pressesToSubtractFromMax;
-        const newTarget = remainingJoltageTarget(
-          [...target],
-          button.button,
-          pressesToTry
-        );
-        if (pressesToTry >= 1) {
-          const val = solveJoltageTargetByDecreasingPressesStrategy(
-            buttonsToTry,
-            [...newTarget],
-            currentButtons + pressesToTry,
-            currentMin,
-            theoreticalMin,
-            false,
-            targetCache
-          );
-          targetCache.set(
-            newTarget.join(","),
-            val - currentButtons - pressesToTry
-          );
-          currentMin = Math.min(currentMin, val);
-          if (currentMin < Infinity) {
-            return currentMin;
-          }
-        }
-      }
-      pressesToSubtractFromMax++;
-    }
-  }
-
+  targetCache.set(targetCacheKey, currentMin);
   return currentMin;
 };
