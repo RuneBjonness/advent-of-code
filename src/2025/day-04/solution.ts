fdd48b75d6167e70d493d16ab52f5d94b6240ce7
Rune Bjønness <rune.bjonness@gmail.com>
2026-01-20 23:33:01 +0100

    2025 day 4: improve performance

--- src/2025/day-04/solution.ts
+++ src/2025/day-04/solution.ts
@@ -1,5 +1,5 @@
 import { AocPuzzle } from "@/aoc-puzzle";
-import { Grid } from "@/lib/grid";
+import { Grid, GridCell } from "@/lib/grid";
 import { adjacentAndDiagonalDeltas } from "@/lib/direction";
 import { ValueGrid } from "@/lib/value-grid";
 
@@ -8,7 +8,7 @@ const silver = (input: string): number => {
   let count = 0;
   for (let row = 0; row < grid.numRows; row++) {
     for (let col = 0; col < grid.numCols; col++) {
-      if (grid.getCellUnsafe({ row, col }) === "@") {
+      if (grid.cells[row * grid.numCols + col] === "@") {
         let adjacentCount = 0;
         for (const delta of adjacentAndDiagonalDeltas) {
           const neighbor = { row: row + delta.row, col: col + delta.col };
@@ -28,41 +28,56 @@ const silver = (input: string): number => {
   return count;
 };
 
-const gold = (input: string): number => {
-  const grid = Grid.fromInput(input, (val) => {
-    return { s: val, adjacentOccupied: -1 };
-  });
-  let count = 0;
-
-  let occupiedCells = grid.cells.filter((c) => c.value.s === "@");
-  let occupiedCellsLastPass = occupiedCells.length + 1;
-
-  while (occupiedCells.length < occupiedCellsLastPass) {
-    occupiedCellsLastPass = occupiedCells.length;
-    for (const cell of occupiedCells) {
-      if (cell.value.adjacentOccupied >= 4) {
-        continue;
-      }
-      const adjacentOccupiedCells = grid
-        .getAdjacentCells(cell, true)
-        .filter((c) => c.value.s === "@");
-
-      if (cell.value.adjacentOccupied < 0) {
-        cell.value.adjacentOccupied = adjacentOccupiedCells.length;
-      }
-      if (cell.value.adjacentOccupied < 4) {
-        count++;
-        cell.value.s = "x";
-        for (const adjacentCell of adjacentOccupiedCells) {
-          if (adjacentCell.value.adjacentOccupied > 0) {
-            adjacentCell.value.adjacentOccupied--;
-          }
-        }
-      }
-    }
-    occupiedCells = occupiedCells.filter((c) => c.value.s === "@");
-  }
-  return count;
+type CellValue = {
+  s: string;
+  adjacentOccupied: number;
 };
 
-export const day04 = new AocPuzzle(2025, 4, silver, gold);
+const gold = (input: string): number => {
+  return both(input)[1];
+};
+
+const both = (input: string): [number, number] => {
+  const grid = Grid.fromInput<CellValue>(input, (val) => {
+    return { s: val, adjacentOccupied: -1 };
+  });
+
+  let toRemove: GridCell<CellValue>[] = [];
+
+  for (const cell of grid.cells.filter((c) => c.value.s === "@")) {
+    const adjacentOccupiedCells = grid
+      .getAdjacentCells(cell, true)
+      .filter((c) => c.value.s === "@");
+
+    cell.value.adjacentOccupied = adjacentOccupiedCells.length;
+
+    if (cell.value.adjacentOccupied < 4) {
+      toRemove.push(cell);
+    }
+  }
+  const silverCount = toRemove.length;
+  let goldCount = 0;
+
+  while (toRemove.length > 0) {
+    const cell = toRemove.pop();
+    if (cell.value.s === "x") {
+      continue;
+    }
+    goldCount++;
+    cell.value.s = "x";
+    const adjacentOccupiedCells = grid
+      .getAdjacentCells(cell, true)
+      .filter((c) => c.value.s === "@");
+
+    for (const adjacentCell of adjacentOccupiedCells) {
+      adjacentCell.value.adjacentOccupied--;
+      if (adjacentCell.value.adjacentOccupied < 4) {
+        toRemove.push(adjacentCell);
+      }
+    }
+  }
+
+  return [silverCount, goldCount];
+};
+
+export const day04 = new AocPuzzle(2025, 4, silver, gold, both);
