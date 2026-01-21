858cb46d9b8372772196b9f3674c4402153a9218
Rune Bjønness <rune.bjonness@gmail.com>
2026-01-21 20:13:17 +0100

    2025 day 5: add combined solution

--- src/2025/day-05/solution.ts
+++ src/2025/day-05/solution.ts
@@ -56,4 +56,45 @@ const gold = (input: string): number => {
   return count;
 };
 
-export const day05 = new AocPuzzle(2025, 5, silver, gold);
+const both = (input: string): [number, number] => {
+  const [rangeSection, idSection] = input.split("\n\n");
+
+  const ranges = rangeSection
+    .split("\n")
+    .map((ranges) => ranges.split("-").map(Number))
+    .sort((a, b) => a[0] - b[0]);
+
+  const ids = idSection
+    .split("\n")
+    .map(Number)
+    .sort((a, b) => a - b);
+
+  let rangeIndex = 0;
+  let silverCount = 0;
+  let goldCount = ranges[rangeIndex][1] - ranges[rangeIndex][0] + 1;
+  let lastMax = ranges[rangeIndex][1];
+
+  for (const id of ids) {
+    if (id >= ranges[rangeIndex][0] && id <= ranges[rangeIndex][1]) {
+      silverCount++;
+    } else {
+      while (rangeIndex < ranges.length - 1 && id > ranges[rangeIndex][1]) {
+        rangeIndex++;
+        if (ranges[rangeIndex][0] > lastMax) {
+          goldCount += ranges[rangeIndex][1] - ranges[rangeIndex][0] + 1;
+          lastMax = ranges[rangeIndex][1];
+        } else if (ranges[rangeIndex][1] > lastMax) {
+          goldCount += ranges[rangeIndex][1] - lastMax;
+          lastMax = ranges[rangeIndex][1];
+        }
+      }
+      if (id >= ranges[rangeIndex][0] && id <= ranges[rangeIndex][1]) {
+        silverCount++;
+      }
+    }
+  }
+
+  return [silverCount, goldCount];
+};
+
+export const day05 = new AocPuzzle(2025, 5, silver, gold, both);
